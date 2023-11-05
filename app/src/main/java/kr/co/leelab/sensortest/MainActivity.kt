b4b99fa31b0d5ff97cd2cfdfabeee86d002043a2
HyeokJae <gurwo63@naver.com>
2023-11-06 00:06:39 +0900

    접근 권한이 설정 되면 센서값이 움직이게 하는 동작 구현 (정확하게 돌아가는지 확인해야 함)

--- app/src/main/java/kr/co/leelab/sensortest/MainActivity.kt
+++ app/src/main/java/kr/co/leelab/sensortest/MainActivity.kt
@@ -1,19 +1,29 @@
 package kr.co.leelab.sensortest
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorManager
 import android.hardware.SensorEventListener
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.CompoundButton
 import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
+    //퍼미션 (권한요청)
+    val MY_PERMISSION_SENSOR = 100
+
+    /*센서동작할 속성들*/
     //가속도 센서
     lateinit var accelerSwitch : Switch //활성 여부
     //각 센서가 움직임을 감지받는 축
@@ -32,6 +42,19 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     lateinit var magnetomX : TextView
     lateinit var magnetomY : TextView
     lateinit var magnetomZ : TextView
+
+    //각 센서 매니저 생성
+    private val acceletorSensor by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+    private val gyroscopeSensor by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+    private val magnetomSensor by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    private var activityPermission : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,38 +78,43 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         magnetomY = findViewById(R.id.magnetomY)
         magnetomZ = findViewById(R.id.magnetomZ)
 
-        //라디오 버튼 식으로 하나만 활성화되게 하기 (다꺼도 상관 없음)
-        accelerSwitch.setOnCheckedChangeListener { p0, isChecked ->
-            if(isChecked) {
-                gyroscopeSwitch.isChecked = false
-                magnetomSwitch.isChecked = false
+        // 퍼미션 확인해주는 데이터
+        val sensorPermission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.BODY_SENSORS)
+        val sensorBackgroundPermission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.BODY_SENSORS_BACKGROUND)
+        if(sensorPermission == PackageManager.PERMISSION_GRANTED || //센서 퍼미션이 권한이 확인되면
+            sensorBackgroundPermission == PackageManager.PERMISSION_GRANTED) {
+            activityPermission = true
+            //라디오 버튼 식으로 하나만 활성화되게 하기 (다꺼도 상관 없음)
+            accelerSwitch.setOnCheckedChangeListener { p0, isChecked ->
+                if (isChecked) {
+                    gyroscopeSwitch.isChecked = false
+                    magnetomSwitch.isChecked = false
+                }
+            }
+            gyroscopeSwitch.setOnCheckedChangeListener { p0, isChecked ->
+                if (isChecked) {
+                    accelerSwitch.isChecked = false
+                    magnetomSwitch.isChecked = false
+                }
+            }
+            magnetomSwitch.setOnCheckedChangeListener { p0, isChecked ->
+                if (isChecked) {
+                    accelerSwitch.isChecked = false
+                    gyroscopeSwitch.isChecked = false
+                }
             }
         }
-        gyroscopeSwitch.setOnCheckedChangeListener { p0, isChecked ->
-            if(isChecked) {
-                accelerSwitch.isChecked = false
-                magnetomSwitch.isChecked = false
-            }
+        else    // 안되있으면 설정 요청
+        {
+            activityPermission = false
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BODY_SENSORS),MY_PERMISSION_SENSOR)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BODY_SENSORS_BACKGROUND),MY_PERMISSION_SENSOR)
         }
-        magnetomSwitch.setOnCheckedChangeListener { p0, isChecked ->
-            if(isChecked) {
-                accelerSwitch.isChecked = false
-                gyroscopeSwitch.isChecked = false
-            }
-        }
-    }
 
-    //각 센서 매니저 생성
-    private val acceletorSensor by lazy {
-        getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    }
-    private val gyroscopeSensor by lazy {
-        getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    }
-    private val magnetomSensor by lazy {
-        getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    }
 
+    }
     override fun onResume(){    //센서 등록 메서드
         super.onResume()
 
@@ -112,38 +140,56 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             )
         }
     }
-
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {    // 센서 정밀도 변경시
     //이 메서드는 onSensorChanged를 사용하기 위해 필요한 함수이다 그리고 메인 클래스에서 SensorEventListener를 상속받아야 한다 (interface 상속)
     }
     override fun onSensorChanged(event: SensorEvent?) {               // 센서 값 변경시
 
-        event?.let {
-            if(accelerSwitch.isChecked) {   //가속도 센서 받아오는 값
-                sensorX.text = event!!.values[0].toString()
-                sensorY.text = event.values[1].toString()
-                sensorZ.text = event.values[2].toString()
+        if(activityPermission) {
+            event?.let {
+                if (accelerSwitch.isChecked) {   //가속도 센서 받아오는 값
+                    sensorX.text = event!!.values[0].toString()
+                    sensorY.text = event.values[1].toString()
+                    sensorZ.text = event.values[2].toString()
+                } else if (gyroscopeSwitch.isChecked) {    //자이로 센서 받아오는 값
+                    gyroscopeX.text = event.values[0].toString()
+                    gyroscopeY.text = event.values[1].toString()
+                    gyroscopeZ.text = event.values[2].toString()
+                } else if (magnetomSwitch.isChecked) {     //중력 센서 받아오는 값
+                    magnetomX.text = event.values[0].toString()
+                    magnetomY.text = event.values[1].toString()
+                    magnetomZ.text = event.values[2].toString()
+                }
+                //Log.d("MainActivity", " x:${event.values[0]}, y:${event.values[1]}, z:${event.values[2]} ")
+                // [0] x축값, [1] y축값, [2] z축값
             }
-            else if(gyroscopeSwitch.isChecked) {    //자이로 센서 받아오는 값
-                gyroscopeX.text = event.values[0].toString()
-                gyroscopeY.text = event.values[1].toString()
-                gyroscopeZ.text = event.values[2].toString()
-            }
-            else if(magnetomSwitch.isChecked) {     //중력 센서 받아오는 값
-                magnetomX.text = event.values[0].toString()
-                magnetomY.text = event.values[1].toString()
-                magnetomZ.text = event.values[2].toString()
-            }
-            //Log.d("MainActivity", " x:${event.values[0]}, y:${event.values[1]}, z:${event.values[2]} ")
-            // [0] x축값, [1] y축값, [2] z축값
         }
     }
-
     // ⑤ 리스너 해제
     override fun onPause() {
         super.onPause()
         acceletorSensor.unregisterListener(this)
         gyroscopeSensor.unregisterListener(this)
         magnetomSensor.unregisterListener(this)
+    }
+
+    //퍼미션 동작
+    override fun onRequestPermissionsResult(    //퍼미션 권한이 해제되면 경고창 표시
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == MY_PERMISSION_SENSOR)
+            if (grantResults.size > 0) {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED)
+                    {
+                        //System.exit(0)
+                        Toast.makeText(this,"퍼미션 권한 해제",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
     }
 }
